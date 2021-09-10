8a69d391ba82d4c773a3d7c1fe15e9fbace74651
Aravindhan V <aravindhan.v@perfios.com>
2021-09-10 17:51:57 +0530

    ADDED: PhoneException class

---
+++ src/main/java/UserRegistration/PhoneException.java
@@ -0,0 +1,15 @@
+package UserRegistration;
+
+public class PhoneException extends Exception{
+	enum ExceptionType {
+		PHONE_INVALID,
+		PHONE_NULL
+	}
+	
+	ExceptionType type;
+	
+	public PhoneException (String message, ExceptionType type) {
+		super(message);
+		this.type = type;
+	}
+}
